d8e1323a818605fff377a7b4db3573315f4d93c1
EDSON MELO RODRIGUES DA LUZ <edson.mrluz@senacsp.edu.br>
2019-11-30 23:36:04 -0300

    Adicionando SqLite

--- Projeto-Mobile/obj/Debug/90/android/src/mono/MonoPackageManager_Resources.java
+++ Projeto-Mobile/obj/Debug/90/android/src/mono/MonoPackageManager_Resources.java
@@ -2,8 +2,13 @@ package mono;
 public class MonoPackageManager_Resources {
 	public static String[] Assemblies = new String[]{
 		/* We need to ensure that "Projeto-Mobile.dll" comes first in this list. */
-		"Projeto-Mobile.dll",
 		"MySql.Data.CF.dll",
+		"SQLite-net.dll",
+		"SQLitePCLRaw.batteries_green.dll",
+		"SQLitePCLRaw.batteries_v2.dll",
+		"SQLitePCLRaw.core.dll",
+		"SQLitePCLRaw.lib.e_sqlite3.dll",
+		"SQLitePCLRaw.provider.e_sqlite3.dll",
 		"Xamarin.Android.Arch.Core.Common.dll",
 		"Xamarin.Android.Arch.Core.Runtime.dll",
 		"Xamarin.Android.Arch.Lifecycle.Common.dll",
